433fa824bfee72746def2c0d78a45b97e2a3bad1
andrealmj <andrealmj@live.com>
2019-02-20 10:17:08 +0800

    completed MVP

--- script.jsx
+++ script.jsx
@@ -1,7 +1,8 @@
 class List extends React.Component {
   constructor(){
     super()
-    this.changeHandler = this.changeHandler.bind( this );
+    this.changeHandler = this.changeHandler.bind(this);
+    this.clickHandler = this.clickHandler.bind(this);
   }
 
   state = {
@@ -9,19 +10,29 @@ class List extends React.Component {
     word : ""
   }
 
-  changeHandler(event){
-    this.setState({word:event.target.value});
-    console.log("change", event.target.value);
+  changeHandler(e){
+    this.setState({word: e.target.value});
+    console.log("change", e.target.value);
+  }
+
+  clickHandler(e) {
+    this.state.list.push(this.state.word);
+    this.setState({word: ""});
   }
 
   render() {
       // render the list with a map() here
 
+      let displayList = this.state.list.map( x => {
+        return <li>{x}</li>
+      })
+
       console.log("rendering");
       return (
         <div className="list">
           <input onChange={this.changeHandler} value={this.state.word}/>
-          <button>add item</button>
+          <button onClick={this.clickHandler}>add item</button><br />
+          {displayList}
         </div>
       );
   }
